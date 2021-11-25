59b651ca5027fdc52f0c7b395efb6b77cc24458f
bukrjobs <bukrjobs@gmail.com>
2021-11-25 15:41:11 +0200

    js

---
+++ lesson 3/claswork/main.js
@@ -0,0 +1,60 @@
+//let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+// 1. перебрати його циклом while
+// let i = 0;
+// while (i < arr.length){
+//     console.log(arr[i]);
+//     i++
+// }
+
+// 2. перебрати його циклом for
+// for (let i = 0; i < arr.length; i++) {
+//     let arrElement = arr[i];
+//     document.write(arrElement);
+//     console.log(arrElement);
+// }
+
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+// let i = 0;
+// while (i < arr.length){
+//     if (arr[i] % 2 !==0) {
+//         console.log(arr[i]);
+//     }
+//         i++;
+// }
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+// for (let i = 0; i < arr.length;i++) {
+//         if (arr[i] % 2 !==0)
+//         console.log(arr[i]);
+// }
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+// let i = 0;
+// while (i < arr.length){
+//     if (arr[i]%2===0) {
+//         console.log(arr[i]);
+//     }
+//         i++;
+// }
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+// for (let i = 0; i < arr.length;i++) {
+//         if (arr[i]%2===0)
+//         console.log(arr[i]);
+// }
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 3 === 0) {
+//         arr[i] = "OKTEN";
+//         console.log(arr);
+//     }
+// }
+
+// 8. вивести масив в зворотньому порядку.
+// for (let i = arr.length; i >= 0; i--){
+//     console.log(arr[i]);
+// };
+
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
